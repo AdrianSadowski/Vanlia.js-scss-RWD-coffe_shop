6231e36d76dc709c53f019f50681e5c66fdd5bb4
Adrian Sadowski <sadow.adrian@gmail.com>
2021-09-16 21:32:27 +0200

    add activatePage

---
+++ src/js/app.js
@@ -0,0 +1,68 @@
+import { select, classNames } from './settings.js';
+
+// TO DO LIST:
+//   dodanie wyświetlania <h1>
+//   dodanie aby widać było {{names}}
+//   ustawienie wszystkiego
+
+const app = {
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        thisApp.activatePage(id);
+
+        window.location.hash = '#/' + id;
+      });
+    }
+
+    
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    //add class "active" to matching links
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    for (let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  
+  },
+
+  init: function() {
+    const thisApp = this;
+
+    thisApp.initPages();
+  },
+};
+
+app.init ();
