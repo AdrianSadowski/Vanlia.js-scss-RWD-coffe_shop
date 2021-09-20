549c99df0c69d7626fab26b927cdc5cb8419b420
Adrian Sadowski <sadow.adrian@gmail.com>
2021-09-20 19:38:26 +0200

    try add contact page

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,9 @@ import Product from './Product.js';
 import { select, classNames, settings, titles } from './settings.js';
 
 // TO DO LIST:
-//   dodanie wyświetlania <h1>
-//   handlebard nie działa !!! :)
-//   dodanie aby widać było {{names}}
-//   ustawienie wszystkiego
+//OTWIETANIE DOMYŚLNE HOME
+// DODANIE PRODUCTS DO HOME
+
 
 const app = {
   initPages: function(){
@@ -18,7 +17,8 @@ const app = {
     
     const idFromHash = window.location.hash.replace('#/', '');
 
-    let pageMatchingHash = thisApp.pages[0].id;
+    let pageMatchingHash = thisApp.pages[1].id;
+    
 
     for (let page of thisApp.pages){
       if(page.id == idFromHash){
@@ -26,6 +26,7 @@ const app = {
         break;
       }
     }
+
 
     thisApp.activatePage(pageMatchingHash);
 
@@ -49,11 +50,13 @@ const app = {
     const thisApp = this;
     console.log(titles[pageId]);
     const titleWrapper = document.querySelector('#pages .title h2');
+
     titleWrapper.innerHTML = titles[pageId];
     //add class "active" to matching links
     for(let page of thisApp.pages){
       page.classList.toggle(classNames.pages.active, page.id == pageId);
     }
+    console.log(thisApp.pages);
 
     for (let link of thisApp.navLinks){
       link.classList.toggle(
@@ -81,7 +84,6 @@ const app = {
 
   initMenu: function(){
     const thisApp = this;
-    console.log(thisApp.data.products);
     for(let productData in thisApp.data.products){
       new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
     }
